4b28f6d0cf54f4b46c35b6ea8dbc3da978d5ddd7
ItzRenzo <ezeeplayz15@gmail.com>
2024-02-21 00:25:13 +0800

    Set to full screen

--- CCE107Project/src/LoginGUI.java
+++ CCE107Project/src/LoginGUI.java
@@ -1,40 +1,86 @@
-import java.awt.EventQueue;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+public class LoginGUI extends JFrame implements ActionListener {
 
-public class LoginGUI extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private Container container = getContentPane();
+    private JLabel userLabel = new JLabel("USERNAME");
+    private JLabel passwordLabel = new JLabel("PASSWORD");
+    private JTextField userTextField = new JTextField();
+    private JPasswordField passwordField = new JPasswordField();
+    private JButton loginButton = new JButton("LOGIN");
+    private JButton resetButton = new JButton("RESET");
+    private JCheckBox showPassword = new JCheckBox("Show Password");
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    LoginGUI() {
+        setLayoutManager();
+        setLocationAndSize();
+        addComponentsToContainer();
+        addActionEvent();
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LoginGUI frame = new LoginGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private void setLayoutManager() {
+        container.setLayout(null);
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public LoginGUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    private void setLocationAndSize() {
+        userLabel.setBounds(50, 150, 100, 30);
+        passwordLabel.setBounds(50, 220, 100, 30);
+        userTextField.setBounds(150, 150, 150, 30);
+        passwordField.setBounds(150, 220, 150, 30);
+        showPassword.setBounds(150, 250, 150, 30);
+        loginButton.setBounds(50, 300, 100, 30);
+        resetButton.setBounds(200, 300, 100, 30);
+    }
 
-		setContentPane(contentPane);
-	}
+    private void addComponentsToContainer() {
+        container.add(userLabel);
+        container.add(passwordLabel);
+        container.add(userTextField);
+        container.add(passwordField);
+        container.add(showPassword);
+        container.add(loginButton);
+        container.add(resetButton);
+    }
 
+    private void addActionEvent() {
+        loginButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        showPassword.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == loginButton) {
+            String userText = userTextField.getText();
+            String pwdText = passwordField.getText();
+            if (userText.equalsIgnoreCase("user") && pwdText.equalsIgnoreCase("12345")) {
+                JOptionPane.showMessageDialog(this, "Login Successful");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
+            }
+        } else if (e.getSource() == resetButton) {
+            userTextField.setText("");
+            passwordField.setText("");
+        } else if (e.getSource() == showPassword) {
+            if (showPassword.isSelected()) {
+                passwordField.setEchoChar((char) 0);
+            } else {
+                passwordField.setEchoChar('*');
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    	LoginGUI login = new LoginGUI();
+        login.setTitle("Login");
+        login.setExtendedState(JFrame.MAXIMIZED_BOTH); // Set to full screen        
+        login.setBounds(10, 10, 370, 600);
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.setResizable(false);
+        login.setVisible(true);
+    }
 }
